a21179e278e625be4e8f503125d22acdefa9d25d
varshith <varshithcool@gmail.com>
2021-06-04 16:41:33 +0530

    Implemented saving on db(firestore) and bot functionality

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const { Telegraf } = require('telegraf');
+const cron = require('node-cron');
 const getStockList = require('./scrapper.js');
+const firebaseApp = require('./model');
+const db = firebaseApp.firestore();
 
 const bot = new Telegraf('1854295897:AAG6dGlue6V-5alxLi4zzOiEBUOh4A4xGaY');
+
+cron.schedule('*/15 9-16 * * 1-5', async () => {
+    try{
+        console.log('Running every 5 mins for intraday')
+        getStockList('https://chartink.com/screener/supertrend-strategy-66','IB')
+        .then( result => console.log(result))
+        .catch(err => console.log(err.message));
+    }catch(e){
+        console.log(err.message)
+    }
+
+})
 
 bot.command('start', (ctx) => {
     
@@ -19,40 +34,63 @@ bot.command('start', (ctx) => {
 })
 
 bot.action('PB', async (ctx) => {
-
-    let result = await getStockList('https://chartink.com/screener/st-strategy-buy-positional','PB')
-    let message = ''
-    if (result){
-        result.map( i => {
-            if (message == ''){
-                message = `${i.stockName}` + '  ------  ' + `${i.price}`
-            }else{
-                let str = `${i.stockName}` + '  ------  ' + `${i.price}`
-                message += '\n' + str;
-            }
-        })
+    try{
+        console.log('Running at 3.35 for positional buy trades')
+        await getStockList('https://chartink.com/screener/st-strategy-buy-positional','PB')
+    }catch(e){
+        console.log(err.message)
     }
-    ctx.deleteMessage();
-    ctx.telegram.sendMessage(ctx.chat.id, message)
+    var message = '';
+    db.collection("positionalbuy").get().then( (querySnapshot) => {
+        
+        if (querySnapshot.docs.length != 0){
+            querySnapshot.forEach( i => {
+                if (i.status == 'N'){
+                    if (message == ''){
+                        message = `${i.stockName}` + '  ------  ' + `${i.price}`
+                    }else{
+                        let str = `${i.stockName}` + '  ------  ' + `${i.price}`
+                        message += '\n' + str;
+                    }
+                }
+            })
 
+            ctx.deleteMessage();
+            ctx.telegram.sendMessage(ctx.chat.id, message)
+        }else{
+            ctx.deleteMessage();
+            ctx.telegram.sendMessage(ctx.chat.id, 'No stocks found')
+        }
+    }).catch(err => console.log(err.message))
+    
 })
 
 bot.action('IB', async (ctx) => {
-    let result = await getStockList('https://chartink.com/screener/supertrend-strategy-66','IB')
-    let message = ''
-    if (result){
-        result.map( i => {
-            if (message == ''){
-                message = `${i.stockName}` + '  ------  ' + `${i.price}`
-            }else{
-                let str = `${i.stockName}` + '  ------  ' + `${i.price}`
-                message += '\n' + str;
-            }
-        })
-    }
-    ctx.deleteMessage();
-    ctx.telegram.sendMessage(ctx.chat.id, message)
+    var message = '';
+    db.collection("intrabuy").get().then( (querySnapshot) => {
+        
+        if (querySnapshot.docs.length != 0){
+            
+            querySnapshot.forEach( doc => {
+                let i = doc.data()
+                
+                if (i.status == 'N'){
+                    if (message == ''){
+                        message = `${i.stockName}` + '  ------  ' + `${i.price}`
+                    }else{
+                        let str = `${i.stockName}` + '  ------  ' + `${i.price}`
+                        message += '\n' + str;
+                    }
+                }
+            })
 
+            ctx.deleteMessage();
+            ctx.telegram.sendMessage(ctx.chat.id, message)
+        }else{
+            ctx.deleteMessage();
+            ctx.telegram.sendMessage(ctx.chat.id, 'No stocks found')
+        }
+    }).catch(err => console.log(err.message))
 })
 
 
